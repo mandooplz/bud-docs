7041b18bd2cddbd55e36bc105294309a7bbf004f
kmw4561@gmail.com <kmw4561@gmail.com>
2025-07-31 17:39:23 +0900

    initial commit 2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import type {ReactNode} from 'react';
 import clsx from 'clsx';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
@@ -8,22 +7,18 @@ import Heading from '@theme/Heading';
 
 import styles from './index.module.css';
 
+
+// View
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
+
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <Heading as="h1" className="hero__title">
-          {siteConfig.title}
+          Bud Platform 개발자 포털
         </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
+        <p className="hero__subtitle">Bud Platform 개발에 필요한 모든 것을 찾아보세요.</p>
       </div>
     </header>
   );
@@ -33,8 +28,8 @@ export default function Home(): ReactNode {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title="개발자 포털"
+      description="Bud Platform 개발자를 위한 공식 문서 포털입니다.">
       <HomepageHeader />
       <main>
         <HomepageFeatures />

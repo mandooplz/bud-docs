d6cb7bedb9a9be310a367e8bb095f6e0409f8d31
kmw4561@gmail.com <kmw4561@gmail.com>
2025-07-31 19:44:39 +0900

    update 3

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -136,7 +136,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Bud-Docs. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
